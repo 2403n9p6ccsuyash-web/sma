332116a2be6a943ecf9018852fe02717d89387c0
Suyash Vish <2403n9p6ccsuyash@viva-technology.org>
2026-03-01 17:30:28 +0530

    Fix utils import extension for Vercel

--- api/delete-society.ts
+++ api/delete-society.ts
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from "./_utils.js";
+import { supabaseAdmin } from "./_utils";
 
 export default async function handler(req: any, res: any) {
     if (req.method !== "POST") {
